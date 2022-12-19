d83a4d815b44b8ec38fa16c0efd3d27fdfd4ad53
Abel Johnson <76398370+Abe0770@users.noreply.github.com>
2022-12-19 18:42:46 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# erm version v1.00
+
+trap 'printf "\n";stop' 2
+
+printf "erm Ver 1.00 \n\n"
+
+if [ "$EUID" -ne 0 ]
+    then 
+    	echo "Permission denied! Please run me as root"
+    	exit
+    else
+	FILE=erm
+	if [ -f "$FILE" ]
+	then
+	    printf "Running install script..."
+	    sudo cp erm /usr/local/bin/erm
+	    printf "\nSuccessfully installed erm\n"
+	else
+	    printf "\nError: File erm not found!\n Make sure all the files are included and run the script in erm directory\n"
+	fi
+fi
